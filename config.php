bd8d286c79d08c66bcd0df8a8de71936f9a0d404
Krystian Surowiec <krystian.surowiec.programista@gmail.com>
2022-10-19 14:43:26 +0200

    first commit

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+    return [
+        'host' => 'budget.krystian-surowiec.profesjonalnyprogramista.pl.mysql.dhosting.pl',
+        'user' => 'pa9ith_budgetkr',
+        'password' => 'xieHoo0Phi5t',
+        'database' => 'ohwei4_budgetkr'
+    ];
